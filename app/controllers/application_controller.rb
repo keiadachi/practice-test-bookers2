86eade65cdeec9a03523de30064562070101aa09
keiadachi <2013edcckeiadachi@gmail.com>
2021-03-24 16:49:27 +0000

    testbookers2

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #deviseが使われる前に以下の内容が実行される
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email]) 
+    #登録に必要なのはemail
+  end
+  
+end
